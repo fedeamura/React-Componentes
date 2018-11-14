5b56685c7390b003a30a706dce45cf46fa949b02
fedeamura <fede.amura@gmail.com>
2018-11-14 10:37:14 -0300

    cambios

--- MiToolbar/styles.js
+++ MiToolbar/styles.js
@@ -1,7 +1,8 @@
 const styles = theme => {
   return {
     toolbar: {
-      paddingRight: 24 // keep right padding when drawer closed
+      paddingLeft: 12,
+      paddingRight: 12 // keep right padding when drawer closed
     },
     toolbarIcon: {
       display: "flex",
@@ -11,8 +12,8 @@ const styles = theme => {
       ...theme.mixins.toolbar
     },
     menuButton: {
-      marginLeft: 12,
-      marginRight: 12
+      // marginLeft: 12,
+      // marginRight: 12
     },
     appBar: {
       zIndex: theme.zIndex.drawer + 2
@@ -71,7 +72,9 @@ const styles = theme => {
     contenedorCargandoVisible: {
       opacity: 1
     },
-    contenedorCuerpo: {},
+    contenedorCuerpo: {
+      flex: 1
+    },
     contenedorTitulo: {
       display: "flex",
       flexDirection: "column",
